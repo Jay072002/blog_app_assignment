5888502a8827bc5d54b3cbffd324d8b436ca6a70
vishal <93590077+JaY072002@users.noreply.github.com>
2023-08-29 12:29:02 +0530

    cors error fixed

--- server/index.js
+++ server/index.js
@@ -1,13 +1,18 @@
 const express = require("express");
 require("dotenv").config();
-
+const router = require("./routes/index")
+const cors = require("cors")
 const app = express();
 
+// express middlewares
 app.use(express.json());
 app.use(cors());
 
-const PORT = process.env.PORT;
+// define app routes
+app.use("/api/v1", router); // Route requests to /api/v1 to the router module
 
+// server setup and db connection build
+const PORT = process.env.PORT;
 (async () => {
     try {
         await connectDB(process.env.MONGO_URI); // Connect to the MongoDB database
--- server/routes/index.js
+++ server/routes/index.js
@@ -3,10 +3,13 @@ const authRouter = require("./auth");
 const userRouter = require("./user");
 const articleRouter = require("./articles");
 
+// auth related routes
 router.use("/auth", authRouter);
 
+// user related routes
 router.use("/user", userRouter);
 
+// article related routes
 router.use("/article", articleRouter)
 
 module.exports = router;
